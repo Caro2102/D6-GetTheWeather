2a1fbed51523ccf00c626b2d16732b96604a1fd4
Carolina Cruz <caro21_cruz@hotmail.com>
2022-10-25 16:59:21 -0500

    Se agregaros variables globales al js

---
+++ script.js
@@ -0,0 +1,10 @@
+var userBtn = document.querySelector('#btn-user');
+var nameInputEl = document.querySelector('#cityname');
+var liContainer = document.querySelector('#card-search');
+var cityForm = document.querySelector('#cityForm');
+var cityContainer = document.querySelector('#city-container');
+var fivedayEl =document.querySelector('#fiveday-header');
+var dayForecast = document.querySelector('#day-forecast');
+var APIKey= "c34e5300156b6ed973d2421c25421b56";
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+citySearch();

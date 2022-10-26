f9f2a19708e166372ba98bc0cf7e141a2a841306
Carolina Cruz <caro21_cruz@hotmail.com>
2022-10-26 01:49:32 -0500

    Se solicito respuesta de API

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ var dayForecast = document.querySelector('#day-forecast');
 var APIKey= "c34e5300156b6ed973d2421c25421b56";
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 citySearch();
+btn();
+//Solicitar respuesta de API
+function getWeather(citynames){
+  cityContainer.classList.remove("d-none");
+  cityContainer.textContent = '';
+    dayForecast.textContent='';
+    nameInputEl.value='';
+    var queryURL="https://api.openweathermap.org/data/2.5/forecast?q="+citynames+"&appid=" + APIKey +"&units=imperial";
+    fetch(queryURL)
+    .then(function (response){
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayCity(data, citynames);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect ');
+    });
+  }
